2be67e15de4f45bc70ce361a7d457f45119e98f2
monster ngamuok <140871214+ucihariskoding@users.noreply.github.com>
2023-09-16 00:28:10 +0700

    Update fungsi.js
    tambahan variabel options utk localestring

--- fungsi.js
+++ fungsi.js
@@ -7,7 +7,17 @@ function konversippp() {
 
     // Lakukan konversi
     var hasilKonversi = usdAmount * nilaiTukar;
+     // Ubah hasilKonversi menjadi string dengan pemisah angka nol
+     var hasilKonversiString = hasilKonversi.toLocaleString('id-ID', options);
+
+    // Buat objek untuk mengatur opsi bahasa ke bahasa Indonesia
+    var options = {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+    }
 
     // Tampilkan hasil konversi di dalam span
-    document.getElementById('hasilKonversi').textContent = 'Rp ' +hasilKonversi.toLocaleString('id-ID') ;
+    document.getElementById('hasilKonversi').textContent = 'Rp ' +hasilKonversiString.toLocaleString('id-ID', options) ;
 }
